8764ffa23cc1d0d63cc95f1a4b8e6f343ee5138b
Tin Tran <tinhardware@gmail.com>
2022-06-03 08:55:17 +0700

    Add resources

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Tin Tran on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
