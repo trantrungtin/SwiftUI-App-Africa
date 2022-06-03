daeb9c310bb46b939cfca3db59691bc799580f5e
Tin Tran <tinhardware@gmail.com>
2022-06-03 09:04:28 +0700

    Add TabView

---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Tin Tran on 03/06/2022.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
