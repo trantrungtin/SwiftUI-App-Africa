8a395190d8abf3a23cc4f814d0998659a8dee74f
Tin Tran <tinhardware@gmail.com>
2022-06-03 23:37:22 +0700

    Add a grid layout in ContentView

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -10,22 +10,67 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    @State private var isGridViewActive: Bool = false
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
     
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) {animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) {animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
+                    }
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                }
+                            }
+                        }
+                        .padding(10)
+                        .animation(.easeIn)
                     }
                 }
             }
             .navigationBarTitle("Arfica", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // MARK: - LIST
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        // MARK: - GRID
+                        Button(action: {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    }
+                }
+            }
         }
     }
 }
